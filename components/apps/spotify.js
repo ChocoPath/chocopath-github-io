a39a8696caa525b4e77031fcf4f27a37529a1d3a
ChocoPath <quayeworsley@gmail.com>
2023-02-07 12:34:22 +0000

    update spotify link

--- components/apps/spotify.js
+++ components/apps/spotify.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Spotify() {
     return (
-        <iframe src="https://open.spotify.com/playlist/6sd3OQHzwG2fWgAEM68id5?si=aa34faa113ef4969" frameBorder="0" title="Spotify" className="h-full w-full bg-ub-cool-grey"></iframe>
+        <iframe src="https://open.spotify.com/embed/playlist/6sd3OQHzwG2fWgAEM68id5" frameBorder="0" title="Spotify" className="h-full w-full bg-ub-cool-grey"></iframe>
     )
 }
 
